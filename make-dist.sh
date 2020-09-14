afd15a6f7292dfb340968d83107c203b99532f17
Lucky Kadam <luckykadam94@gmail.com>
2020-09-14 10:13:30 +0530

    add make-dist.sh

---
+++ make-dist.sh
@@ -0,0 +1,10 @@
+if [ -z "$1" ]
+  then
+    echo "No argument supplied. Usage: ./make-dist v1.0"
+    exit 1
+fi
+
+echo "preparing release $1"
+
+addon_zip="Quick Arch $1 - Do not Unzip!.zip"
+zip -r "$addon_zip" qarch
